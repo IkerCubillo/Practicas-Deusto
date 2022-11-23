6e84c7054b85854d479afa3b1e3bc502375bb7e7
Iker Cubillo <Iker Cubillo@IK-2019-554>
2022-11-23 09:19:16 +0100

    a

--- es.deusto.prog3/Practica3_BaseDatos/src/BaseDatos.java
+++ es.deusto.prog3/Practica3_BaseDatos/src/BaseDatos.java
@@ -8,18 +8,19 @@ public class BaseDatos {
 	public static void main(String[] args) {
 		try {
 			Class.forName( "org.sqlite.JDBC" );
-			Connection conn = DriverManager.getConnection( "jdbc:sqlite:mibasededatos.db" );
+			Connection conn = DriverManager.getConnection( "jdbc:sqlite:res/NOMEKOP_Data_Base.db" );
 			Statement statement = conn.createStatement();
 			
-			statement.executeUpdate( "drop table if exists persona" );  // SOLO PORQUE ES PRUEBA!!!!!
-			statement.executeUpdate( "create table persona (id integer, nombre string)" );
+//			statement.executeUpdate( "drop table if exists persona" );  // SOLO PORQUE ES PRUEBA!!!!!
+			statement.executeUpdate( "create table Usuario (id integer, nombre string)" );
+			statement.executeUpdate( "create table Personaje (id integer, nombre string)" );
 			
 			// Create - insert
 			statement.executeUpdate( "insert into persona values (1, 'Andoni')" );
 			int ret = statement.executeUpdate( "insert into persona (id, nombre) values (2, 'Olatz')" );
 			statement.executeUpdate( "insert into persona values (3, 'Emilio')" );
 			System.out.println( "Devuelve " + ret );
-			
+			statement.executeUpdate( "insert into persona values (3, 'Iker')" );
 			// Update - update
 			statement.executeUpdate( "update persona set nombre = 'Aitziber' where id = 2" );
 			
