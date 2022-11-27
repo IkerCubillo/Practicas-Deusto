47bd11fd783dbcfc48f3560427f1f69d9b2ce22b
IkerCubillo <i.cubillo@opendeusto.es>
2022-11-27 19:49:15 +0100

    Unificacion

--- es.deusto.prog3/Practica4_Recursividad/src/Recursividad.java
+++ es.deusto.prog3/Practica4_Recursividad/src/Recursividad.java
@@ -1,11 +1,15 @@
 
 public class Recursividad {
 	public static void main(String[] args) {
-//		System.out.println(invertirFrase("Esta funcion invierte la frase recursivamente", ""));
-//		System.out.println(invertirFrase("Andoni Eguiluz es mi profesor de Programación", ""));
-//		System.out.println(invertirFrase("Las patatas fritas existen para untarse en el huevo", ""));
+		System.out.println(invertirFraseSabin("Esta funcion invierte la frase recursivamente"));
+		System.out.println(invertirFraseSabin("Andoni Eguiluz es mi profesor de Programación"));
+		System.out.println(invertirFraseSabin("Las patatas fritas existen para untarse en el huevo"));
 		
-		System.out.println(invertirPalabras("Patata; que tal: hey - g", args));
+		System.out.println(invertirFraseCubillo("Esta funcion invierte la frase recursivamente", ""));
+		System.out.println(invertirFraseCubillo("Andoni Eguiluz es mi profesor de Programación", ""));
+		System.out.println(invertirFraseCubillo("Las patatas fritas existen para untarse en el huevo", ""));
+		
+//		System.out.println(invertirPalabras("Patata; que tal: hey - g", args));
 	}
 
 	private static String invertirPalabras(String fraseInicial, String[] camposReturn ) {
@@ -14,24 +18,22 @@ public class Recursividad {
 		return campos[0] + "" + campos[1] + "" +campos[2] + ""+ campos[3];
 	}
 
-	private static String invertirFrase(String fraseInicial, String fraseReturn) {
+	private static String invertirFraseCubillo(String fraseInicial, String fraseReturn) {
 		if(fraseInicial.length() == 0 ) {
 			return fraseReturn;
 		} else {
 			fraseReturn += fraseInicial.charAt(fraseInicial.length()-1);
 			fraseInicial = (String) fraseInicial.subSequence(0, fraseInicial.length()-1);
-			System.out.println(fraseReturn);
-			System.out.println(fraseInicial);
-			return invertirFrase(fraseInicial, fraseReturn);
+			return invertirFraseCubillo(fraseInicial, fraseReturn);
 		}
 	}
 	
-	public static String invertirFrase(String palabra) {
+	public static String invertirFraseSabin(String palabra) {
         if (palabra.length() <= 1) {
             return palabra;
         } else {
-        	System.out.println(palabra);
-            return invertirFrase(palabra.substring(1)) + palabra.charAt(0);
+//        	System.out.println(palabra);
+            return invertirFraseSabin(palabra.substring(1)) + palabra.charAt(0);
         }
     }
 	
